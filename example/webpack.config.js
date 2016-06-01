1d940d7a61bb8b1e95a220d69ebdab636e8efa68
xyz <xiaoyuze88@gmail.com>
2016-06-01 22:07:52 +0800

    update to v1.1.14

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -2,53 +2,89 @@
 
 // Modules
 var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ignoreFiles = new webpack.IgnorePlugin(/\.\/jquery-2.1.4.js$/);
 var path = require('path');
-var host = 'localhost';
-var port = 3000;
 
-module.exports = {
-  host: host,
-  port: port,
-  entry    : {
+var config = {
+  context: path.resolve(__dirname, './client'),
+  entry: {
     app: [
-      './example/client/index.js'
+      'webpack-dev-server/client?http://localhost:3000/',
+      path.resolve(__dirname, './client/index')
     ]
   },
-  output   : {
-    path: __dirname + '/public',
-    publicPath: 'http://' + host + ':' + port + '/',
+  output: {
+    // path: 'http://localhost:3000/',
+    path: path.resolve(__dirname, './dist'),
+    publicPath: 'http://localhost:3000/',
     filename: 'static/js/[name].bundle.js',
-    chunkFilename: 'static/js/[name].bundle.js'
+    // chunkFilename: 'static/js/[name].bundle.js'
   },
-  devtool  : 'inline-source-map',
-  module   : {
-    preLoaders: [],
-    loaders   : []
-  },
-  plugins  : [
-    new HtmlWebpackPlugin({
-      template: './example/client/index.html',
-      inject  : 'body',
-      chunks  : ['app']
-    }),
-    new webpack.optimize.DedupePlugin()
-  ],
-  devServer: {
-    contentBase: './public',
-    stats: {
-      modules: false,
-      cached : false,
-      colors : true,
-      chunk  : false
-    },
-    proxy: {
-      '/api/*': {
-        target: 'http://' + host + ':' + (port + 1),
-        secure: false,
-        bypass: function (req, res, proxyOptions) {
-        }
+  devtool: 'eval',
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      },
+      {
+        test: /\.(jpg|jpeg|gif|png)/,
+        loader: 'url-loader?name=static/images/[name].[ext]&limit=10240'
       }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './client/index.html'),
+      inject: 'body',
+      chunks: ['app'],
+      minify: false
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
+
+var compiler = webpack(config);
+
+var serverOptions = {
+  // contentBase: 'http://localhost:3000',
+  publicPath: 'http://localhost:3000/',
+  // publicPath: path.resolve(__dirname, './dist'),
+
+  contentBase: './public',
+  // hot: true,
+  quiet: true,
+  noInfo: true,
+  inline: true,
+  lazy: false,
+  headers: { 'Access-Control-Allow-Origin': '*' },
+  stats: {
+    modules: false,
+    cached: false,
+    colors: true,
+    chunk: false
+  },
+  proxy: {
+    '/upload': {
+      target: 'http://localhost:3002',
+      secure: false,
+      // bypass: function (req, res, proxyOptions) {
+      //   if (req.headers.accept.indexOf('html') !== -1) {
+      //     console.log('Skipping proxy for browser request.');
+      //     return '/index.html';
+      //   }
+      // }
     }
   }
 };
+
+var server = new WebpackDevServer(compiler, serverOptions);
+
+server.listen(3000, function(err) {
+  if (err) throw err;
+
+  console.log('webpack dev server is listening to port 3000. Open http://localhost:3000 to see the example.');
+});
+
